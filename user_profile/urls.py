38c42103c4ad712a5c3f213f7f478dbfb1be2c80
Nikita <gargnikita1511@gmail.com>
2024-03-14 16:34:06 +0530

    edit profile page introduced

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from . import views
+from .views import edit_profile
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    path('edit-profile/', edit_profile, name='edit_profile'),
     # Other URL patterns for your application
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,9 +1,22 @@
 # views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+from django.contrib import messages
 
 @login_required
 def profile(request):
     user = request.user
     return render(request, 'user_profile/profile.html', {'user': user})
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST,request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated successfully!')
+            return redirect('profile')  # Redirect to user profile page after saving
+    else:
+        form = UserProfileForm(instance=request.user)
+    return render(request, 'user_profile/edit_profile.html', {'form': form})
